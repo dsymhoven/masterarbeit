4ec63c929f6a108a497fbab2f55bca789daa198c
David Symhoven <symhoven@googlemail.com>
2016-09-17 12:15:45 +0200

    creates Xcode Project References #1 Closed #1

---
+++ masterarbeit/masterarbeit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  masterarbeit
+//
+//  Created by David Symhoven on 17.09.16.
+//  Copyright © 2016 David Symhoven. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
