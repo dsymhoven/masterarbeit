40aa499ceb67357716850d16a700fb300c7b4057
David Symhoven <symhoven@googlemail.com>
2016-11-16 16:32:27 +0100

    Adds new python script for plotting nearField of Particle Adds new postProcessing bash script #8

---
+++ Analysis/plot_nearField.py
@@ -0,0 +1,61 @@
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+files = glob.glob('Particles/*.txt')
+gridParameters = np.genfromtxt('gridParameters.txt')
+field = np.genfromtxt('E_fields/E_field0.txt')
+
+lengthOfSimulationBoxInX = gridParameters[0]
+lengthOfSimulationBoxInY = gridParameters[1]
+numberOfGridPointsInX = gridParameters[3]
+numberOfGridPointsInY = gridParameters[4]
+
+numberOfBoxesInX = numberOfGridPointsInX / lengthOfSimulationBoxInX
+numberOfBoxesInY = numberOfGridPointsInY / lengthOfSimulationBoxInY
+
+lengthOfOneBoxInX = lengthOfSimulationBoxInX / numberOfBoxesInX
+lengthOfOneBoxInY = lengthOfSimulationBoxInY / numberOfBoxesInY 
+
+
+for file in files:
+	# read data from text and save it into array data
+	data = np.genfromtxt(file)
+	# open figure
+	fig = plt.figure()
+	# plot x and y value of particle as red dot
+	plt.plot(data[0][1], data[0][2], marker='o', color = 'r')
+	plt.imshow(field, vmin=0, vmax=1.0)
+	plt.colorbar()
+	
+	currentAxis = plt.gca()
+	rect = patches.Rectangle(
+        (10, 10),	# bottom left corner
+        20,			# width
+        20,			# heigth
+        linewidth = 1,
+        edgecolor = 'r',
+        fill=False  # remove background
+    )
+	currentAxis.add_patch(rect)
+    
+	# set labels
+	plt.xlabel("X")
+	plt.ylabel("Y")
+	# set axis 
+	plt.xlim([0, lengthOfSimulationBoxInX])
+	plt.ylim([0, lengthOfSimulationBoxInY])
+	plt.xticks(np.arange(0, lengthOfSimulationBoxInX + 1, lengthOfOneBoxInX))
+	plt.yticks(np.arange(0, lengthOfSimulationBoxInY + 1, lengthOfOneBoxInY))
+	
+	#plt.grid(linestyle='-', color='red')
+	# define filename for saving
+	filename = file.replace(".txt", "")
+	filename = filename.replace("Particles/", "")
+	fig.savefig("png/" + "{}.png".format(filename), bbox_inches='tight')
+	# close fig
+	plt.close(fig)
+	
+
