b9dfae01d8023b7766d3a6f2c7c79f88c83774a7
David Symhoven <symhoven@googlemail.com>
2016-11-23 19:32:59 +0100

    adds new bash-script and python plot script for calculatingLWFieldsForPlane Deletes some movies #5

---
+++ Analysis/Scripts/particlesAndFieldsForPlane.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# define variables
+PATHTOANALYSIS=~/Desktop/Projects/masterarbeit/Analysis
+PATHTOEXECUTABLE=~/Library/Developer/Xcode/DerivedData/masterarbeit-gqaflsvzotvzahddhxaxmsryswvq/Build/Products/Debug
+
+# check if file with name "B_field0.txt" exists in /Library/Developer/Xcode/DerivedData/masterarbeit-gqaflsvzotvzahddhxaxmsryswvq/Build/Products/Debug/
+# if so, create new folder "B_Fields" in Analysis folder of masterarbeit directory and copy all B_field files to that folder
+# analogue with E field and Particle files
+# if gridParameters were written out, copy them into Analysis folder. folder
+if [ -e B_field0.txt ]
+then
+	mkdir -p $PATHTOANALYSIS/B_fields
+	mv $PATHTOEXECUTABLE/B_field*.txt $PATHTOANALYSIS/B_Fields
+fi
+if [ -e E_field0.txt ]
+then
+	mkdir -p $PATHTOANALYSIS/E_fields
+	mv $PATHTOEXECUTABLE/E_field*.txt $PATHTOANALYSIS/E_Fields
+fi
+if [ -e Particle0.txt ]
+then
+	mkdir -p $PATHTOANALYSIS/Particles
+	mv $PATHTOEXECUTABLE/Particle*.txt $PATHTOANALYSIS/Particles
+fi
+if [ -e gridParameters.txt ]
+then
+	mv $PATHTOEXECUTABLE/gridParameters.txt $PATHTOANALYSIS/
+fi
+#  create png folder in Analysis folder. Neccessary for python script
+mkdir -p $PATHTOANALYSIS/png
+# change directory to Analysis folder in order for python script to work properly
+cd $PATHTOANALYSIS
+# execute python script
+python2.7 plot_particlesAndFieldsForPlane.py
--- Analysis/plot_particlesAndFieldsForPlane.py
+++ Analysis/plot_particlesAndFieldsForPlane.py
@@ -7,7 +7,7 @@ import os
 
 numberOfParticleFiles = len(fnmatch.filter(os.listdir('Particles/'), '*.txt'))
 
-#files = glob.glob('Particles/*.txt')
+files = glob.glob('Particles/*.txt')
 gridParameters = np.genfromtxt('gridParameters.txt')
 #field = np.genfromtxt('E_fields/E_field0.txt')
 
@@ -20,33 +20,36 @@ numberOfBoxesInY =  lengthOfSimulationBoxInY / lengthOfOneBoxInY
 x=[]
 y=[]
 
+field = np.genfromtxt('E_fields/E_field0.txt')
+
 for i in range(numberOfParticleFiles):
 	# read data from text and save it into array data
 	data = np.genfromtxt('Particles/Particle'+ str(i) +'.txt')
-	field = np.genfromtxt('E_fields/E_field'+ str(i) +'.txt')
-	# open figure
-	fig = plt.figure()
+	
 	# define variables
 	x.append(data[0][1])
 	y.append(data[0][2])
-	# plot x and y value of particle as red dot
-	plt.plot(x, y, color = 'r')
-	# plot fields
-	plt.imshow(field, aspect='auto', origin='lower', extent=(0,lengthOfSimulationBoxInX,0,lengthOfSimulationBoxInY), vmin=0, vmax=0.05)
-	plt.colorbar()
-	# set labels
-	plt.xlabel("X")
-	plt.ylabel("Y")
-	# set axis 
-	plt.xlim([0, lengthOfSimulationBoxInX])
-	plt.ylim([0, lengthOfSimulationBoxInY])
-	plt.xticks(np.arange(0, lengthOfSimulationBoxInX + 1, lengthOfOneBoxInX))
-	plt.yticks(np.arange(0, lengthOfSimulationBoxInY + 1, lengthOfOneBoxInY))
-	plt.grid(linestyle = "-", color='red')
-	# define filename for saving
-	filename = 'img' + str(i)
-	fig.savefig("png/" + "{}.png".format(filename), bbox_inches='tight')
-	# close fig
-	plt.close(fig)
+
+# open figure
+fig = plt.figure()
+# plot x and y value of particle as red dot
+plt.plot(x, y, color = 'r')
+# plot fields
+plt.imshow(field, aspect='auto', origin='lower', extent=(0,lengthOfSimulationBoxInX,0,lengthOfSimulationBoxInY), vmin=0, vmax=0.05)
+plt.colorbar()
+# set labels
+plt.xlabel("X")
+plt.ylabel("Y")
+# set axis 
+plt.xlim([0, lengthOfSimulationBoxInX])
+plt.ylim([0, lengthOfSimulationBoxInY])
+plt.xticks(np.arange(0, lengthOfSimulationBoxInX + 1, lengthOfOneBoxInX))
+plt.yticks(np.arange(0, lengthOfSimulationBoxInY + 1, lengthOfOneBoxInY))
+plt.grid(linestyle = "-", color='red')
+# define filename for saving
+filename = 'img'
+fig.savefig("png/" + "{}.png".format(filename), bbox_inches='tight')
+# close fig
+plt.close(fig)
 	
 
