4c2b40f50cb406eaf62981882bee6b0aea22bb86
David Symhoven <symhoven@googlemail.com>
2016-12-07 09:43:12 +0100

    Modifies python plot script. Now at max 40 positions of particle trajectory get plotted #11

--- Analysis/plot_particlesAndFields.py
+++ Analysis/plot_particlesAndFields.py
@@ -29,6 +29,9 @@ for i in range(numberOfParticleFiles):
 	# define variables
 	x.append(data[0][1])
 	y.append(data[0][2])
+	if len(x) > 40:
+		x.pop(0)
+		y.pop(0)
 	# plot x and y value of particle as red dot
 	plt.plot(x, y, color = 'r')
 	# plot fields
